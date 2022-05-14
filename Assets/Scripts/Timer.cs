85bd8fef4eb59636af2a5cc15c7383ab738739bb
Artem Grabovsky <comradegrabovsky@gmail.com>
2022-05-14 15:14:07 +0500

    Added new models and animations. Clean the mess in Project Tab

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public int Seconds = 180;
+    public Text TimerPanel;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Tick), 0, 1);
+    }
+
+    private void Tick()
+    {
+        Seconds -= 1;
+
+        string minutes = (Seconds / 60).ToString();
+        string seconds = (Seconds % 60).ToString();
+
+        if (minutes.Length == 1) minutes = "0" + minutes;
+        if (seconds.Length == 1) seconds = "0" + seconds;
+
+        TimerPanel.text = $"{minutes}:{seconds}";
+
+        if (Seconds == 0)
+        {
+            TimerPanel.color = Color.red; 
+            CancelInvoke(nameof(Tick));
+        }
+    }
+}
